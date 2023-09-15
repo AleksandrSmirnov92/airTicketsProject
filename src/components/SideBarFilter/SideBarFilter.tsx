1f978085dd33dc17462ca3c688794791c86ff33f
AleksandrSmirnov92 <ryan00@mail.ru>
2023-09-15 12:06:31 +0300

    deploy

--- src/components/SideBarFilter/SideBarFilter.tsx
+++ src/components/SideBarFilter/SideBarFilter.tsx
@@ -171,7 +171,9 @@ const SideBarFilter: React.FC<SideBarProps> = ({
               className={style["section-card__form"]}
               onChange={(e) =>
                 setFirstNumber((prev) =>
-                  Number(prev) > 200001 ? "" : e.target.value
+                  Number(prev) > 200001 && /^[0-9]*$/.test(e.target.value)
+                    ? ""
+                    : e.target.value
                 )
               }
             />
@@ -187,7 +189,9 @@ const SideBarFilter: React.FC<SideBarProps> = ({
               min={0}
               onChange={(e) =>
                 setSecondNumber((prev) =>
-                  Number(prev) > 200001 ? "" : e.target.value
+                  Number(prev) > 200001 && /^[0-9]*$/.test(e.target.value)
+                    ? ""
+                    : e.target.value
                 )
               }
             />
